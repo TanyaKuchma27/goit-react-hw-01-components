aafd089e36b7bf0812cac35c33d7ab48fc642717
Tanya Kuchma <kuchma.tanya@ukr.net>
2022-02-17 21:53:39 +0200

    task 1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import user from './user.json';
+import Profile from './components/Profile/Profile';
+
+export const App = () => {
+  return (
+    <div>
+      <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      followers={user.stats.followers}
+      views={user.stats.views}
+      likes={user.stats.likes}
+    />
+    </div>
+   
+  );
+};
