7dd00ad6b786d7470034406d09b748266f15b3e0
Tanya Kuchma <kuchma.tanya@ukr.net>
2022-02-21 13:48:49 +0200

    Update App

--- src/comps/App.jsx
+++ src/comps/App.jsx
@@ -1,11 +1,11 @@
-import user from 'data/user.json';
-import stats from 'data/stats.json';
-import friends from 'data/friends.json';
-import transactions from 'data/transactions.json';
 import Profile from 'comps/Profile/Profile';
 import Statistics from 'comps/Statistics/Statistics';
 import FriendList from 'comps/FriendList/FriendList';
 import TransactionHistory from 'comps/TransactionHistory/TransactionHistory';
+import user from 'data/user.json';
+import stats from 'data/stats.json';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
 
 export default function App() {
   return (
